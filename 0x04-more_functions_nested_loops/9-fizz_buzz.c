96686081722eec9d88f467090944f9ccb61a6ca5
seanjoe45 <seanjoe45@gmail.com>
2022-09-18 18:59:55 +0100

    Update 9-fizz_buzz.c

--- 0x04-more_functions_nested_loops/9-fizz_buzz.c
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -15,7 +15,11 @@ int main(void)
 	{
 		if ((n % 3 == 0) && (n % 5 == 0))
 		{
-			printf("Fizz")
+			printf(" FizzBuzz");
+		}
+		else if (n % 3 == 0)
+		{
+			printf(" Fizz");
 		}
 		else if (n % 5 == 0)
 		{
