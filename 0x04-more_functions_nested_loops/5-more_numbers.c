1daf482c00228bd7bcf33269902cc5eb7460068b
seanjoe45 <seanjoe45@gmail.com>
2022-09-18 19:17:43 +0100

    Update 5-more_numbers.c

--- 0x04-more_functions_nested_loops/5-more_numbers.c
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -15,10 +15,16 @@ void more_numbers(void)
 		for (ch = 0; ch < 15; ch++)
 		{
 			if (ch <= 10)
-				_putchar ((ch / 10) + 48);
-			_putchar((ch % 10) + 48)
+				_putchar ('0' + ch);
 		}
+		else
+		{
+			_putchar('0' + ch / 10);
+			_putchar('0' + j % 10);
+		}
+		ch++;
 		_putchar('\n');
+		i++;
 	}
 }
 
