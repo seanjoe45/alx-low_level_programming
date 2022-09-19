3535cfcf751b4a7f677190c29fc429018593344b
seanjoe45 <seanjoe45@gmail.com>
2022-09-19 21:17:38 +0100

    Update 6-puts2.c

--- 0x05-pointers_arrays_strings/6-puts2.c
+++ 0x05-pointers_arrays_strings/6-puts2.c
@@ -16,13 +16,14 @@ void puts2(char *str)
 }
 
 /**
- * string_length - finds the length of a string.
- * Return: length of c.
- * @pointer: pointer.
- */
+  * string_length - finds the length of a string.
+  * Return: length of c.
+  * @pointer: pointer.
+  */
 int string_length(char *pointer)
 {
 	int c = 0;
+
 	while (*(pointer + c) != '\0')
 	{
 		c++;
