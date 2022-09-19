2738238d35eb671cae3ca1c5dc73c902a893c16c
seanjoe45 <seanjoe45@gmail.com>
2022-09-19 21:07:01 +0100

    Update 7-puts_half.c

--- 0x05-pointers_arrays_strings/7-puts_half.c
+++ 0x05-pointers_arrays_strings/7-puts_half.c
@@ -17,7 +17,7 @@ voids puts_half(char *str)
 			_putchar(str[i]);
 		}
 	}
-	else if (string_length(str) % 2 i= 0)
+	else if (string_length(str) % 2 != 0)
 	{
 		n = (string_length(str) - 1) / 2;
 		for (j = n; j <= string_length(str); j++)
@@ -25,7 +25,7 @@ voids puts_half(char *str)
 			_putchar(str[j]);
 		}
 	}
-	_putchar('\n')
+	_putchar('\n');
 }
 /**
  * string_length - finds the length of a string.
@@ -34,7 +34,7 @@ voids puts_half(char *str)
  */
 int string_length(char *pointer)
 {
-	int c = 0
+	int c = 0;
 		while(*(pointer + c) != '\0')
 		{
 			c++;
