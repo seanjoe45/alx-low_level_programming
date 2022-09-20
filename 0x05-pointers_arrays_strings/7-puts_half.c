41a9f80da2dd1a9f647c72e7f1b37e06c4da0987
seanjoe45 <seanjoe45@gmail.com>
2022-09-20 02:11:27 -0700

    Update 7-puts_half.c

--- 0x05-pointers_arrays_strings/7-puts_half.c
+++ 0x05-pointers_arrays_strings/7-puts_half.c
@@ -1,44 +1,25 @@
 #include "main.h"
+
 /**
- * puts_half - print half of a string.
- * @str: character
+ * puts_half - prints the second half of a given string
+ * @str: passed string pointer
+ *
+ * Return: void
  */
 void puts_half(char *str)
 {
-	int m, n, i, j;
+	int len = 0;
 
-	m = string_length(str) / 2;
-
-	if (string_length(str) % 2 == 0)
+	while (*(str + len) != '\0')
+		len++;
+	if (len % 2 == 0)
+		len /= 2;
+	else
+		len = len / 2 + 1;
+	while (*(str + len) != '\0')
 	{
-		for (i = m; i < string_length(str); i++)
-		{
-			_putchar(str[i]);
-		}
-	}
-	else if (string_length(str) % 2 != 0)
-	{
-		n = (string_length(str) - 1) / 2;
-		for (j = n; j <= string_length(str); j++)
-		{
-			_putchar(str[j]);
-		}
+		_putchar(*(str + len));
+		len++;
 	}
 	_putchar('\n');
-}
-
-/**
-  * string_length - finds the length of a string.
-  * Return: length of c.
-  * @pointer: pointer.
-  */
-int string_length(char *pointer)
-{
-	int c = 0;
-
-	while (*(pointer + c) != '\0')
-	{
-		c++;
-	}
-	return (c);
 }
