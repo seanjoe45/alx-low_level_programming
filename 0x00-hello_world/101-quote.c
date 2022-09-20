5471285e8991e2b67e5a31785a1bcffc26457446
seanjoe45 <seanjoe45@gmail.com>
2022-09-20 08:17:11 -0700

    101-quote.c

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 #include <stdio.h>
 #include <unistd.h>
 
@@ -18,24 +17,3 @@ write(2, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", 59);
 			return (1);
 
 }
-=======
-#include <stdio.h>
-#include <unistd.h>
-
-/**
-* main - Entry point
-*
-* Return: Always 1 (Success)
-*
-*/
-
-int main(void)
-
-{
-
-	write(2, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", 59);
-
-	return (1);
-
-}
->>>>>>> 221fad154d7b8ef6c4f9d32a0f03ec7af8ef3f72
