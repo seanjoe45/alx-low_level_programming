c9ba564b85bd0db3ff9bf2f209b9b7a295ee4bac
seanjoe45 <seanjoe45@gmail.com>
2022-09-23 10:36:00 -0700

    Update 7-leet.c

--- 0x06-pointers_arrays_strings/7-leet.c
+++ 0x06-pointers_arrays_strings/7-leet.c
@@ -11,18 +11,19 @@ char *leet(char *s)
 	int low_letters[] = {97, 101, 111, 116, 108};
 	int upp_letters[] = {65, 69, 79, 84, 76};
 	int numbers[] = {52, 51, 48, 55, 49};
-
+ 
 	while (*(s + count) != '\0')
 	{
 		for (i = 0; i < 5; i++)
 		{
 			if (*(s + count) == low_letters[i] || *(s + count) == upp_letters[i])
 			{
-			*(s + count) == numbers[i];
-			break;
+				*(s + count) = numbers[i];
+				break;
 			}
 		}
 		count++;
 	}
+
 	return (s);
 }
