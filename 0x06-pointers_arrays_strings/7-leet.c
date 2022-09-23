60c01f1d1281b8fdbf8aa344066cad6dcd7bcd01
seanjoe45 <seanjoe45@gmail.com>
2022-09-23 10:52:27 -0700

    7-leet.c

--- 0x06-pointers_arrays_strings/7-leet.c
+++ 0x06-pointers_arrays_strings/7-leet.c
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 #include "main.h"
 /**
  * leet - encodes a string into 1337
@@ -13,40 +12,11 @@ char *leet(char *s)
 	int upp_letters[] = {65, 69, 79, 84, 76};
 	int numbers[] = {52, 51, 48, 55, 49};
 
-	while (*(s + count) != '\0')
+	while (*s(s + count) != '\0')
 	{
 		for (i = 0; i < 5; i++)
 		{
-			if (*(s + count) == low_letters[i] || *(s + count) == upp_letters[i])
-			{
-			*(s + count) == numbers[i];
-			break;
-			}
-		}
-		count++;
-	}
-	return (s);
-}
-=======
-#include "main.h"
-/**
- * leet - encodes a string into 1337
- * @s: input string.
- * Return: the pointer to dest.
- */
-
-char *leet(char *s)
-{
-	int count = 0, i;
-	int low_letters[] = {97, 101, 111, 116, 108};
-	int upp_letters[] = {65, 69, 79, 84, 76};
-	int numbers[] = {52, 51, 48, 55, 49};
-
-	while (*(s + count) != '\0')
-	{
-		for (i = 0; i < 5; i++)
-		{
-			if (*(s + count) == low_letters[i] || *(s + count) == upp_letters[i])
+			if (*(s + count) == low_letters[] || *(s + count) == upp_letters[i])
 			{
 				*(s + count) = numbers[i];
 				break;
@@ -55,6 +25,5 @@ char *leet(char *s)
 		count++;
 	}
 
-	return (s);
+	return (s)
 }
->>>>>>> 834dfca19c4a389432a6b3db31ad8f2a8b5ee3a4
