b4632585ed5085a17ec85f278186db7af436129b
seanjoe45 <seanjoe45@gmail.com>
2022-09-24 04:24:12 +0100

    Update 104-print_buffer.c

--- 0x06-pointers_arrays_strings/104-print_buffer.c
+++ 0x06-pointers_arrays_strings/104-print_buffer.c
@@ -25,7 +25,7 @@ void print_buffer(char *b, int size)
 				if (k < size)
 					printf("%.2x", *(b + k));
 				else
-					printf(" ");
+					printf("  ");
 			}
 			printf(" ");
 			for (l = j; l < j + 10; l++)
@@ -33,9 +33,9 @@ void print_buffer(char *b, int size)
 				if (l >= size)
 					break;
 				if (*(b + l) < 32 || *(b + l) > 126)
-					printf("%c" '.');
+					printf("%c", '.');
 				else
-					printf("%c", *(b + 1));
+					printf("%c", *(b + l));
 			}
 			printf("\n");
 		}
