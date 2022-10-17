9df7b08176c7dabcbfe5b12a201827a4a8f4ee4a
seanjoe45 <seanjoe45@gmail.com>
2022-10-17 05:06:39 -0700

    printed_characters.c

---
+++ printf/printed_characters.c
@@ -0,0 +1,19 @@
+#include "main.h"
+#include <stdio.h>
+#include <string.h>
+
+/**
+ * _printf - function that produces output according to format
+ * @s - the character string
+ * @d - the integers to be returned
+ * @format - the string to be printed
+ * Return: returns the number of characters printed.
+ */
+int _printf(const char *format, ...);
+{
+	char *str = "format";
+
+	printf("%d\n", strlen(str));
+
+	return (0);
+}
