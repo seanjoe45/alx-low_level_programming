3309ddb07571c47c1200f24fbf5614dd57b1bf22
seanjoe45 <seanjoe45@gmail.com>
2022-10-27 08:33:01 -0700

    103-find_loop.c

--- 0x13-more_singly_linked_lists/4-free_listint.c
+++ 0x13-more_singly_linked_lists/4-free_listint.c
@@ -1,4 +1,4 @@
-#include "lists.h"
+#include "list.h"
 
 /**
  * free_listint - frees a linked list
@@ -7,12 +7,5 @@
  * Return: no return.
  */
 void free_listint(listint_t *head)
-{
-	listint_t *temp;
 
-	while ((temp = head) != NULL)
-	{
-		head = head->next;
-		free(temp);
-	}
-}
+
