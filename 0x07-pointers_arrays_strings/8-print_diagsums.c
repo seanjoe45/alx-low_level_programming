0f657e18295a0f56c774c8783287f14637012962
seanjoe45 <seanjoe45@gmail.com>
2022-10-30 06:52:55 -0700

    Update 8-print_diagsums.c

--- 0x07-pointers_arrays_strings/8-print_diagsums.c
+++ 0x07-pointers_arrays_strings/8-print_diagsums.c
@@ -1,23 +1,12 @@
 #include "main.h"
-#include <stdio.h>
 
 /**
- * print_diagsums - prints the sum of the two diagnols of square matrix.
- *
- * @a: pointer to array element.
- * @size: size of matrix.
- * Return: nothing.
+ * set_string - sets the value of a pointer to a char
+ * @s: source adress.
+ * @to: target adress.
+ * Return: no return.
  */
-void print_diagsums(int *a, int size)
+void set_string(char **s, char *to)
 {
-	int m, sum, sum1;
-
-	sum = 0;
-
-	for (m = 0; m < size; m++)
-		sum += *(a + ((size * m) + m));
-	sum1 = 0;
-	for (m = 1; m <= size; m++)
-		sum1 += *(a + ((size * m) - m));
-	printf("%d, %d\n", sum, sum1);
+	*s = to;
 }
