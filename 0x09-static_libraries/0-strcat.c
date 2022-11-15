2687f8c0fecb4b618a6ba215defeac059cc4552a
seanjoe45 <seanjoe45@gmail.com>
2022-11-15 08:35:27 -0800

    Update 0-strcat.c

--- 0x09-static_libraries/0-strcat.c
+++ 0x09-static_libraries/0-strcat.c
@@ -1,9 +1,9 @@
 #include "main.h"
 /**
- *  _strcat - concatenates two strings,
- *  @dest: destination.
- *  @src: source.
- *  Return: the pointer to dest.
+ * _strcat - concatenates two strings,
+ * @dest: destination.
+ * @src: source.
+ * Return: the pointer to dest.
  */
 char *_strcat(char *dest, char *src)
 {
